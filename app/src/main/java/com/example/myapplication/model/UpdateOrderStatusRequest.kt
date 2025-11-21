0aab54b628580306516a4b3a8edea58b9cc313eb
Emmvnu3l <emm.moreno@duocuc.cl>
2025-11-20 22:29:56 -0300

    mejora de ordenes

--- app/src/main/java/com/example/myapplication/model/UpdateOrderStatusRequest.kt
+++ app/src/main/java/com/example/myapplication/model/UpdateOrderStatusRequest.kt
@@ -3,5 +3,7 @@ package com.example.myapplication.model
 import com.google.gson.annotations.SerializedName
 
 data class UpdateOrderStatusRequest(
-    @SerializedName("status") val status: String
+    @SerializedName("status") val status: String,
+    @SerializedName("reason") val reason: String? = null,
+    @SerializedName("admin_id") val adminId: Int? = null
 )
